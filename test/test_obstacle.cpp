05f4338f7879dea7683ee9c438908c291ac881aa
shivang <shivaang14@gmail.com>
2018-10-15 18:37:47 -0400

    [test][#15] Updated test for Obstacle_generator.

--- test/test_obstacle.cpp
+++ test/test_obstacle.cpp
@@ -8,15 +8,23 @@
  */
 #include <gtest/gtest.h>
 #include <obstacle_generator.hpp>
-
 /**
  * @brief     A general test, for testing all the methods of the class.
  */
-TEST(testObstacleClass, allInOneTest) {
-  Obstacle_generator obs;
+TEST(testObstacleClass, allVarTest) {
+  Obstacle_generator obs(std::make_pair(0, 0), std::make_pair(799, 0), true);
   EXPECT_GE(obs.get_height(), 0);
   EXPECT_GE(obs.get_width(), 0);
   EXPECT_GE(obs.get_goalpoint().first, 0);
   EXPECT_GE(obs.get_startpoint().second, 0);
-  EXPECT_FALSE(obs.generate_obstacles().empty());
+}
+/**
+ * @brief     A general test, for testing all the methods of the class.
+ */
+TEST(testObstacleClass, loopTest) {
+  Obstacle_generator obs;
+  obs.generate_obstacles();
+  obs.run_bfs();
+  obs.run_sdl();
+  EXPECT_EQ(obs.get_height(), 600);
 }

f4a2328939b88abab464defae5bb8859336364df
shivang <shivaang14@gmail.com>
2018-10-15 20:06:31 -0400

    [impl][#17] Implemented main.cpp to simulate execution.

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,21 @@
+/**
+ * @file      main.cpp
+ * @author    Shivang Patel
+ * @copyright MIT license
+ *
+ * @brief DESCRIPTION
+ *
+ * This files is the main file which creates
+ * object of Obstacle_generator class.
+ *
+ */
 #include <iostream>
-#include <lib.hpp>
+#include <obstacle_generator.hpp>
 
-int main()
-{
-    dummy();
-    return 0;
+int main() {
+  Obstacle_generator obs(std::make_pair(0, 599),
+      std::make_pair(799, 0), true);            // Making object.
+  obs.generate_obstacles();                     // Creating obstacle.
+  obs.run_bfs();                                // BFS
+  obs.run_sdl();                                // Showing the outpur.
 }
